97a89645b8ec7bff3643b80a1a3caa15cef6f383
CincoDeMayo13 <76203416+CincoDeMayo13@users.noreply.github.com>
2022-12-18 16:39:48 +0800

    Initial commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -174,29 +174,18 @@
         delay: 5000,
         disableOnInteraction: false
       },
-      slidesPerView: 'auto',
       pagination: {
         el: '.swiper-pagination',
         type: 'bullets',
         clickable: true
       },
       breakpoints: {
-        320: {
-          slidesPerView: 2,
-          spaceBetween: 40
-        },
-        480: {
+       
+        1080: {
           slidesPerView: 3,
-          spaceBetween: 60
-        },
-        640: {
-          slidesPerView: 4,
-          spaceBetween: 80
-        },
-        992: {
-          slidesPerView: 6,
-          spaceBetween: 120
+          spaceBetween: 100
         }
+       
       }
     });
   
